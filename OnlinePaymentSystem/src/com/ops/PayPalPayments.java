307d5b935420379fda84b0a4a744f992860fea55
Tanu Verma <tanu77688@gmail.com>
2023-09-20 14:31:53 +0530

    new project payment app

---
+++ OnlinePaymentSystem/src/com/ops/PayPalPayments.java
@@ -0,0 +1,7 @@
+package com.ops;
+
+public class PayPalPayments {
+	public void test2() {
+		System.out.println("Paypal done");
+	}
+}
---
+++ OnlinePaymentSystem/src/com/ops/UPIPayment.java
@@ -0,0 +1,7 @@
+package com.ops;
+
+public class UPIPayment {
+	public void test1() {
+		System.out.println("UPI done");
+	}
+}
